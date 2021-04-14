a35718ca50e355fee2b84100dde656ff92329ca4
PawelSkrodzki <pawel.skrodzki14@gmail.com>
2021-04-14 21:43:49 +0200

    graind fixed

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ setInterval(showTime, 1000)
          animate: true,
          patternWidth: 100,
          patternHeight: 100,
-         grainOpacity: 0.05,
+         grainOpacity: 0.08,
          grainDensity: 1,
          grainWidth: 1,
          grainHeight: 1
