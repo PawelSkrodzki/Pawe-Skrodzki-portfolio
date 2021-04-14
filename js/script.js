04504ce4d0178548fe5a41e7fa099e75e266c524
PawelSkrodzki <pawel.skrodzki14@gmail.com>
2021-04-14 20:55:35 +0200

    background effect added

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ showTime = () =>{
     yearSpan.innerHTML = `${moment().format("YYYY")}`
 }
 setInterval(showTime, 1000)
+
+ var options = {
+         animate: true,
+         patternWidth: 100,
+         patternHeight: 100,
+         grainOpacity: 0.03,
+         grainDensity: 1,
+         grainWidth: 1,
+         grainHeight: 1
+ };
+
+
+grained('#main', options);
